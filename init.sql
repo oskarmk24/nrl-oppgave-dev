3801ef6860487247367da4d39fc41c709206e3f7
oskarmk24 <oskarmk@student.uia.no>
2025-10-01 19:11:27 +0200

    oppdatert compose filen og lagt til en sql fil knyttet til db tabellen.

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Reports (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    ObstacleName VARCHAR(255),
+    ObstacleHeight DECIMAL(10,2),
+    ObstacleDescription TEXT,
+    ObstacleLocation VARCHAR(255)
+);
